574c50debd4273081fece0940f6191d4eb72a43c
aarenhou <hsian.lung@gmail.com>
2025-10-03 16:16:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -354,4 +354,4 @@ def recording_status():
     return jsonify(status)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, threaded=True, host='127.0.0.1', port=10000)
